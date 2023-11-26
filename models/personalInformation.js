556ee7e0a7bd80109e9015500d4b5a8cf06ba6bd
priyanka <priyankaenggg1111@gmail.com>
2023-11-27 01:08:23 +0530

    hello

--- models/personalInformation.js
+++ models/personalInformation.js
@@ -9,56 +9,36 @@ const PersonalInformation = mongoose.model(
         dateOfBirth: String,
         gender: String,
         maritalStatus: String,
-
-
         mailingAddressOne:String,
         mailingAddressTwo:String,
         mailingCountry:String,
         mailingState:String,
         mailingCity:String,
         mailingpincode:String,
-     
-
         permanentAddressOne:String,
         permanentAddressTwo:String,
         permanentCountry:String,
         permanentState:String,
         permanentCity:String,
         permanentpincode:String,
-      
-
         passportNumber:String,
         issueDate:String,
         expiryDate:String,
         issueCountry:String,
         birthCity:String,
         birthCountry:String,
-
-
-
         nationality:String,
         citizenship:String,
         moreCountryCitizenship:String,
         moreCountryLiving:String,
-
-
-
-
         migrationCountry:String,
         medicalCondition:String,
         visaRefusal:String,
         criminal:String,
-
-
-        name:String,
-        phone:String,
-        email:String,
-        relationWithApplicant:String,
-
-
-
-
-
+        emergencyname:String,
+        emergencyphone:String,
+        emergencyemail:String,
+        emergencyrelationWithApplicant:String,
     })
 );
 module.exports = PersonalInformation;
