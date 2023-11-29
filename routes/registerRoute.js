475e6b7858ae96d3d353a33d05e1ac33bae64aa9
priyanka <priyankaenggg1111@gmail.com>
2023-11-29 08:35:59 +0530

    ll

--- routes/registerRoute.js
+++ routes/registerRoute.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var agentController = require('../controllers/agentController.js');
 var studentController = require('../controllers/studentController.js');
 var {verifyAgentToken} = require('../middleware/auth')
-
+//gg
 router.post('/',verifyAgentToken,studentController.create);
 
 module.exports = router;
