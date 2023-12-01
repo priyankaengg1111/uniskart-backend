928ae0bd9fe82845872e3c30fa380d507fe36742
priyankaengg1111 <96824655+priyankaengg1111@users.noreply.github.com>
2023-12-01 22:44:06 +0530

    sdfdf

--- controllers/workExperienceController.js
+++ controllers/workExperienceController.js
@@ -9,6 +9,7 @@ module.exports = {
     create: async function (req, res) {
         const { organizationName, position, jobProfile, workingFrom, workingUpto, salaryMode, workingHere } = req.body;
         var admin = new workExperienceModel({
+            studentId:req.params.id,
             organizationName,
             position,
             jobProfile,
@@ -20,6 +21,24 @@ module.exports = {
         let result =await admin.save();
         return res.status(201).json(result);
     },
+    show:async function(req,res){
+        try{
+            const allData = await workExperienceModel.findOne({
+                studentId:req.params.id,
+            });
+            if (allData!=null) {
+                res.status(200).json({ 
+                    status:true,
+                    data: allData });
+            }else{
+                res.status(200).json({ 
+                    status:true,
+                    data: 'noData' });
+            }
+        }catch(err){
+            res.status(500).json(err);
+        }
+    }
     /**
      * adminController.remove()
      */
--- routes/workExperienceRoute.js
+++ routes/workExperienceRoute.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var workExperienceController = require('../controllers/workExperienceController.js');
 
 
-router.post('/',workExperienceController.create);
+router.post('/:id',workExperienceController.create);
+router.get('/:id',workExperienceController.show);
+
 
 module.exports = router;
