8ec4096f8f4b6beb009ed1e5251e7281ab47014a
priyankaengg1111 <96824655+priyankaengg1111@users.noreply.github.com>
2023-12-02 19:41:48 +0530

    xcxc

--- controllers/testController.js
+++ controllers/testController.js
@@ -67,6 +67,54 @@ module.exports = {
         res.status(500).json(err);
       }
     },
+    update: async function (req, res) {
+        try {
+            console.log("firstid", req.params.id)
+            const { greOverallScore,greExaminationDate,greQuantitative,greVerbal,greAnalyticalWriting,gmatOverallScore,gmatExaminationDate,gmatQuantitative,gmatVerbal,gmatAnalyticalWriting,gmatIntegratedReasoning,toeflOverallScore,toeflExaminationDate,toeflReading,toeflListening,toeflSpeaking,toeflWriting,ieltsYetReceive,ieltsTestResultDate,ieltsTRFNo,ieltsIELTSWaiver,ieltsEnglishTwelvethMark,ieltsExaminationDate,ieltsReading,ieltsListening,ieltsSpeaking,ieltsWriting,ieltsOverallScore,pteOverallScore,pteExaminationDate,pteReading,pteListening,pteSpeaking,pteWriting,detOverallScore,detExaminationDate,satOverallScore,satExaminationDate,satReading,satMath,satEssay,actOverallScore,actExaminationDate,actMath,actReading,actScience,actEnglish,actWriting} = req.body;
+            const result2 = await testModel.find(
+                { studentId: req.params.id });
+            console.log("result2", result2)
+            const result = await testModel.updateOne(
+                { studentId: req.params.id }, // Specify the condition to identify the document to update
+                {
+                    $set: {
+                        greOverallScore,greExaminationDate,greQuantitative,greVerbal,greAnalyticalWriting,gmatOverallScore,gmatExaminationDate,gmatQuantitative,gmatVerbal,gmatAnalyticalWriting,gmatIntegratedReasoning,toeflOverallScore,toeflExaminationDate,toeflReading,toeflListening,toeflSpeaking,toeflWriting,ieltsYetReceive,ieltsTestResultDate,ieltsTRFNo,ieltsIELTSWaiver,ieltsEnglishTwelvethMark,ieltsExaminationDate,ieltsReading,ieltsListening,ieltsSpeaking,ieltsWriting,ieltsOverallScore,pteOverallScore,pteExaminationDate,pteReading,pteListening,pteSpeaking,pteWriting,detOverallScore,detExaminationDate,satOverallScore,satExaminationDate,satReading,satMath,satEssay,actOverallScore,actExaminationDate,actMath,actReading,actScience,actEnglish,actWriting
+                    }
+                }
+            );
+            // if (result.nModified > 0) {
+                return res.status(201).json({ message: 'Document updated successfully.' });
+            // } else {
+            //     console.log("great error")
+            //     return res.status(404).json({ message: 'No document found or no modifications made.' });
+            // }
+        } catch (err) {
+            console.log("myer", err);
+            return res.status(500).json({
+                error: err.message
+            });
+        }
+    },
+    show: async function (req, res) {
+        try {
+            const allData = await testModel.findOne({
+                studentId: req.params.id,
+            });
+            if (allData != null) {
+                res.status(200).json({
+                    status: true,
+                    data: allData
+                });
+            } else {
+                res.status(200).json({
+                    status: true,
+                    data: 'noData'
+                });
+            }
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
     /**
      * adminController.remove()
      */
--- routes/testRoute.js
+++ routes/testRoute.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var testController = require('../controllers/testController.js');
 
 
-router.post('/',testController.create);
+router.post('/:id',testController.create);
+router.put('/:id',testController.update);
+
+router.get('/:id',testController.create);
+
 
 module.exports = router;
